00a5077ebbfe641220bbe9e5a5e3b5f5e0bc5de9
fedexu <federico.xu125@gmail.com>
2020-02-03 12:00:45 +0100

    test PWA list on device

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { LoaderInterceptorService } from './shared/loader/loader-interceptor.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,7 @@ import { LoaderInterceptorService } from './shared/loader/loader-interceptor.ser
     BrowserModule,
     AppRoutingModule,
     HttpClientModule, 
-    SharedModule
+    SharedModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     {
