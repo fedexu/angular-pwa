9a51686ab03307f440edc3e077265cff321d47ec
fedexu <federico.xu125@gmail.com>
2020-02-05 09:09:39 +0100

    restored graphicis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from './api.service';
-import { RouterOutlet } from '@angular/router';
-import { fadeInOutRoutingAnimation } from './routing.animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    fadeInOutRoutingAnimation()
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
-  constructor(
-    private readonly apiService: ApiService
-  ) { }
+  constructor() { }
 
   ngOnInit() { }
 
-  animation(outlet: RouterOutlet) {
-    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
-  }
 
 }
